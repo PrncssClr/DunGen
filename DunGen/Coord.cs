3a9d99481cb6a4dceda7e0443f4eecd71111687d
Jake Watkins <jkwtkns@live.com>
2019-01-26 13:55:27 -0600

    Added Coord class

---
+++ DunGen/Coord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DunGen
+{
+	class Coord
+	{
+		private int x, y;
+		public int GetX() { return x; }
+		public void SetX(int _x) { x = _x; }
+		public int GetY() { return y; }
+		public void SetY(int _y) { y = _y; }
+
+		public Coord(int _x, int _y) { x = _x; y = _y; }
+
+		public static Coord operator +(Coord a, Coord b) { return new Coord(a.GetX() + b.GetX(), a.GetY() + b.GetY()); }
+		public static Coord operator -(Coord a, Coord b) { return new Coord(a.GetX() - b.GetX(), a.GetY() - b.GetY()); }
+	}
+}
---
+++ DunGen/Tile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DunGen
+{
+	class Tile
+	{
+		private Coord coord;
+		public Coord GetCoord() => coord;
+		public void SetCoord(Coord c) { coord = c ?? throw new ArgumentNullException(nameof(c)); }
+
+	}
+}
